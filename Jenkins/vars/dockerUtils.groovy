8772bf5e1a7b4477857fdbb60a315873704727d3
Srujeeth Reddy <srujeeth@Srujeeths-MacBook-Air.local>
2025-07-13 19:37:43 +0530

    changes file

--- Jenkins/vars/dockerUtils.groovy
+++ Jenkins/vars/dockerUtils.groovy
@@ -1,16 +1,14 @@
 def buildAndPushDockerImage(String imageName) {
     sh """
         docker build -t ${imageName} .
-        echo "Fake login: skipping real DockerHub login"
         docker tag ${imageName} ${imageName}
-        echo "Fake push: skipping actual push"
     """
 }
 
 def runHealthcheck(String imageName) {
     sh """
         docker run -d --name http-echo -p 5678:5678 ${imageName}
-        sleep 3
-        curl -f http://localhost:5678 || echo "Health check failed"
+        sleep 5
+        curl -f http://localhost:5678 || echo 'Healthcheck failed'
     """
 }
