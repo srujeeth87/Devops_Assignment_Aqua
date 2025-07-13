3ee9b883069d4e616a8f6d03f8e96c4447670fa2
srujeeth87 <39269914+srujeeth87@users.noreply.github.com>
2025-07-13 22:33:05 +0530

    Update dockerUtils.groovy

--- Jenkins/vars/dockerUtils.groovy
+++ Jenkins/vars/dockerUtils.groovy
@@ -1,9 +1,15 @@
 def buildAndPushDockerImage(String imageName) {
+    withCredentials([usernamePassword(credentialsId: '4a0089bb-e1b8-4ded-8f6a-3562c48483b1', usernameVariable: 'DOCKERHUB_USER', passwordVariable: 'DOCKERHUB_PASS')]) 
+           {
     sh """
+        echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin 
         docker build -t ${imageName} .
         docker tag ${imageName} ${imageName}
+        docker push ${imageName}
     """
+           }
 }
+
 
 def runHealthcheck(String imageName) {
     sh """
